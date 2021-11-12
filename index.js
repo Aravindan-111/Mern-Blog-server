244d6f939f5625b7f23f7f1aa0c9a4f993b94aff
Aravindan-111 <aravindhan11195@gmail.com>
2021-11-12 08:50:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+
+const router = require("./routes/routes");
+const MONGO_URL =
+  "mongodb+srv://aravindan:admin123@test-cluster-1.cho6i.mongodb.net/blog?retryWrites=true&w=majority";
+
+// mongoose connect
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("connection successfull");
+  })
+  .catch((err) => {
+    console.log("error", err);
+  });
+app.use(express.json());
+// middlewares
+app.use("/", router);
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log("Server running at port 3001");
+});
